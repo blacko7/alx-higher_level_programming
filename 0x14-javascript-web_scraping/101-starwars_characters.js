5bc76d553c99642f9ff4b5349f2dc74a4f37647a
blacko7 <bgyawu@gmail.com>
2024-06-04 14:34:56 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,32 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(url, (error, response, body) => {
+request(url, (error, response) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
+    console.error(error);
+    return;
+  }
+
+  const characters = JSON.parse(response.body).characters;
+  const characterPromises = characters.map(character => {
+    return new Promise((resolve, reject) => {
+      request(character, (error, response) => {
         if (error) {
-          console.log(error);
+          reject(error);
         } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+          resolve(JSON.parse(response.body).name);
         }
       });
-    }
-  }
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
